db2d8b4bb78fd75efcfff98185f7f26fdde1cb73
sanket <sanket.naik@luxactuaries.com>
2021-03-22 16:44:58 +0530

    robots added

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -9,3 +9,19 @@ Disallow: /api/*`);
         res.end();
     }
 }
+
+// import React, { Component } from "react";
+// import fetch from 'isomorphic-unfetch';
+
+// export default class SiteMap extends Component {
+//     static async getInitialProps({req, res}) {
+//         if(res) {
+//             const response = await fetch(`http://${req.headers['host']}/api/sitemap`);
+//             const text = await response.text();
+//             res.setHeader("Content-Type", "text/xml");
+//             res.write(text);
+//             res.end();
+//         }
+
+//     }
+// }
