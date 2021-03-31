0a4c9e929cde8adaf8314e4b208e5fa8892de4e7
sanket <sanket.naik@luxactuaries.com>
2021-03-31 12:18:57 +0530

    chnages

--- components/loader/Loader.js
+++ components/loader/Loader.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import LazyImage from '../utils/LazyImage'
 
-export default function Loader() {
+export default function Loader(props) {
     return (
         <div className="d-flex Loader">
             <img id="loadingL" src="https://res.cloudinary.com/dlmozkbdc/image/upload/v1616682625/SkillSchool/loadingIcon-removebg-preview_yasaa4.png"/>
-            <div className="loadertxt">&nbsp;Loading...</div>
+            <div className="loadertxt">&nbsp;{props.children}</div>
         </div>
     )
 }
